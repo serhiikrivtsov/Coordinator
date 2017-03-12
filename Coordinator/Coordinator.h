c972c31bccc8593666d74d912bfddf6230aa6076
Serhiy Krivtsov <krivcov.serhiy@gmail.com>
2017-03-12 21:20:09 +0200

    Initial Commit

---
+++ Coordinator/Coordinator.h
@@ -0,0 +1,19 @@
+//
+//  Coordinator.h
+//  Coordinator
+//
+//  Created by Serhii Krivtsov on 3/12/17.
+//  Copyright © 2017 Serhii Krivtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coordinator.
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+
+//! Project version string for Coordinator.
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordinator/PublicHeader.h>
+
+
